a5cb1c29b6bb9104a68a0919fb8f1037e267f372
7Bssk7 <arhip.fin3@gmail.com>
2025-09-25 19:30:08 -0700

    Update

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <array>
 #include <fstream>
+#include <string>
 using namespace std;
+
+class Movie{
+    public:
+        string getTitle() {}
+        void setTitle() {}
+        int getYear() {}
+        void setYear() {}
+        string screenWriter() {}
+        void screenWrite() {}
+
+    private:
+        string title;
+        int year;
+        string screenWriter;
+       
+
+};
 
 int main(){
 
